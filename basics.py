23574ab827d64757c92d62bb0a5fad70033b54e1
SirGuamsalot <56278042+Gcc07@users.noreply.github.com>
2026-01-09 12:57:30 +0000

    Tweaked code to pass pytest checks in basics.py and broken_module.py

--- basics.py
+++ basics.py
@@ -3,6 +3,8 @@ def calculate_discount(price, is_member):
     Returns the discounted price.
     Members receive a 10% discount.
     """
-    if is_member:
-        return price - 0.10
+    if price < 0:
+        raise ValueError
+    if is_member:   
+        return price - (price * .1)
     return price
